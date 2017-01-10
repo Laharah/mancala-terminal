f71a8204da64ce1c4999b5482719e10b8be9afe0
laharah <laharah22@gmail.com>
2017-01-10 12:15:35 -0800

    Added basic board class with tests

---
+++ mancala/board.py
@@ -0,0 +1,43 @@
+'simulates a board for mancala'
+import itertools
+from collections import namedtuple
+
+Score = namedtuple("Score", "TOP, BOTTOM")
+State = namedtuple("State", "top, bottom, turn")
+
+class Board:
+    TOP=0
+    BOTTOM=1
+    def __init__(self, state=None, seeds=4):
+        'players will be top and botom'
+        self.top = [0]+ [seeds]*6
+        self.bottom = [0] + [seeds]*6
+        self.turn = self.TOP
+
+    def get_state(self):
+        return State(tuple(self.top), tuple(self.bottom), self.turn)
+
+    @classmethod
+    def from_state(cls, state):
+        board = cls()
+        board.top = list(state.top)
+        board.bottom = list(state.bottom)
+        board.turn = state.turn
+        return board
+
+    def swap_turn(self):
+        self.turn += 1
+        self.turn %= 2
+
+    @property
+    def top_store(self):
+        return self.top[0]
+
+    @property
+    def bottom_store(self):
+        return self.bottom[0]
+
+
+    @property
+    def score(self):
+        return Score(self.top_store, self.bottom_store)
---
+++ mancala/test/test_board.py
@@ -0,0 +1,53 @@
+import pytest
+from ..board import Board
+
+@pytest.fixture
+def sample_board():
+    b = Board()
+    b.top[2] = 0
+    for i in range(3, 7):
+        b.top[i] += 1
+    b.swap_turn()
+    return b
+
+def test_create():
+    b = Board()
+    assert b.top == b.bottom == [0, 4, 4, 4, 4, 4, 4]
+
+def test_swap_turn():
+    b = Board()
+    t = b.turn
+    b.swap_turn()
+    assert b.turn != t
+    b.swap_turn()
+    assert b.turn == t
+
+def test_stores():
+    b = Board()
+    assert b.top_store == 0
+    assert b.bottom_store == 0
+
+def test_score():
+    b = Board()
+    score = b.score
+    assert score == (0, 0)
+    assert score.TOP == 0
+    assert score.BOTTOM == 0
+
+def test_get_state():
+    b = Board()
+    initial_side = (0, 4, 4, 4, 4, 4, 4)
+    assert b.get_state() == (initial_side, initial_side, b.TOP)
+
+def test_state_hashable(sample_board):
+    d = {}
+    d[0] = sample_board.get_state()
+    assert d[0] == sample_board.get_state()
+
+def test_from_state(sample_board):
+    state = sample_board.get_state()
+    b = Board.from_state(state)
+    assert b is not sample_board
+    assert b.top == sample_board.top
+    assert b.bottom == sample_board.bottom
+    assert b.turn == sample_board.turn
