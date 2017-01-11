ab1ec3680debd4f065356834042c9e0229dff2d5
laharah <laharah22@gmail.com>
2017-01-10 19:17:10 -0800

    added the do_move lodgic to state, switched TOP and BOTTOM
      - changed TOP and BOTTOM for logic reasons
      - score now references top and bottom correctly

--- mancala/board.py
+++ mancala/board.py
@@ -2,20 +2,20 @@
 import itertools
 from collections import namedtuple
 
-Score = namedtuple("Score", "TOP, BOTTOM")
+Score = namedtuple("Score", "top, bottom")
 State = namedtuple("State", "top, bottom, turn")
 
 class State:
     '''an extension of a namedtuple like object. Board becomes a mutable wrapper for easy
     interaction and progression. Also allows for easy dependancy injection through
     multiple inheratance'''
-    TOP = 0
-    BOTTOM = 1
+    TOP = 1
+    BOTTOM = 0
 
     def __init__(self, seeds=4, *, top=None, bottom=None, turn=None):
         self.top = tuple(top) if top else tuple([seeds] * 6 + [0])
         self.bottom = tuple(bottom) if bottom else tuple([seeds] * 6 + [0])
-        self._turn = turn if turn else self.TOP
+        self._turn = turn if turn else self.BOTTOM
 
     @property
     def turn(self):
@@ -24,14 +24,17 @@ class State:
     def __getitem__(self, index):
         return (self.top, self.bottom, self.turn)[index]
 
+    def __repr__(self):
+        return '{}(bottom={}, top={}, turn={})'.format(self.__class__, self.bottom, self.top, self.turn)
+
 
     def __eq__(self, other):
         if not isinstance(other, State):
             other = State.from_tuple(other)
-        cmps = {self.top:other.top,
-                self.bottom: other.bottom}
+        c1 = self.top, self.bottom
+        c2 = other.top, other.bottom
         if self.turn == other.turn:
-            for s, o in cmps.items():
+            for s, o in zip(c1, c2):
                 if all(a == b for a,b in itertools.zip_longest(s, o)):
                     return True
         return False
@@ -72,6 +75,51 @@ class State:
     def next_turn(self):
         return self.BOTTOM if self.turn is self.TOP else self.TOP
 
+    def after_move(self, move):
+        'move is the index that player self.turn wants to use. yields the resulting state'
+        if self.turn == self.TOP:
+            move += 7
+        
+        new_vals = list(itertools.chain(self.bottom, self.top))
+        i_cycle = itertools.cycle(range(len(new_vals)))
+        if self.turn == self.TOP:
+            i_cycle = filter(lambda v: v != 7, i_cycle)
+        else:
+            i_cycle = filter(lambda v: v != 13, i_cycle)
+
+        i_cycle = itertools.dropwhile(lambda i: i <= move, i_cycle)
+        seeds = new_vals[move]
+        new_vals[move] = 0
+        
+        while seeds:
+            index = next(i_cycle)
+            new_vals[index] += 1
+            seeds -= 1
+
+        if index != 6 and index != 13:
+            new_turn = self.next_turn()
+        else:
+            new_turn = self.turn
+
+        if new_vals[index] == 1:
+            opposite = self._opposing_house(index)
+            if 0 < index < 6 and self.turn == self.BOTTOM:
+                new_vals[index] += new_vals[opposite]
+                new_vals[opposite] = 0
+
+            elif 7 < index < 13 and self.turn == self.TOP:
+                new_vals[index] += new_vals[opposite]
+                new_vals[opposite] = 0
+        
+        return State(bottom=new_vals[:7], top=new_vals[7:], turn=new_turn)
+
+    @staticmethod
+    def _opposing_house(index):
+        offset = abs(6-index)
+        if index < 6:
+            return 6 + offset
+        else:
+            return 6 - offset
 
 class Board(State):
     'a mutable wrapper over State'
@@ -108,6 +156,9 @@ class Board(State):
         self.top = list(state.top)
         self.bottom = list(state.bottom)
         self.turn = state.turn
+
+    def __call__(self, move):
+        self._apply(super().after_move(move))
 
 
     def swap_turn(self):
--- mancala/test/test_board.py
+++ mancala/test/test_board.py
@@ -5,9 +5,9 @@ from ..board import Board
 @pytest.fixture
 def sample_board():
     b = Board()
-    b.top[2] = 0
-    for i in range(3, 7):
-        b.top[i] += 1
+    b.bottom[1] = 0
+    for i in range(2, 6):
+        b.bottom[i] += 1
     b.swap_turn()
     return b
 
@@ -36,14 +36,14 @@ def test_score():
     b = Board()
     score = b.score
     assert score == (0, 0)
-    assert score.TOP == 0
-    assert score.BOTTOM == 0
+    assert score.top == 0
+    assert score.bottom == 0
 
 
 def test_get_state():
     b = Board()
     initial_side = (4, 4, 4, 4, 4, 4, 0)
-    assert b.get_state() == (initial_side, initial_side, b.TOP)
+    assert b.get_state() == (initial_side, initial_side, b.BOTTOM)
 
 
 def test_state_hashable(sample_board):
@@ -63,3 +63,32 @@ def test_from_state(sample_board):
     assert b.top == sample_board.top
     assert b.bottom == sample_board.bottom
     assert b.turn == sample_board.turn
+
+def test_call(sample_board):
+    b1 = Board()
+    b1(1)
+    print(b1)
+    print(sample_board)
+    assert b1 == sample_board
+    b1(2)
+    assert b1.score.top == 1
+    assert b1.turn == b1.TOP
+    b1(3)
+    assert b1.score.top == 2
+    assert b1.turn == b1.BOTTOM
+    assert b1.score.bottom == 0
+    assert b1.bottom[:2] == [5, 1]
+    assert b1.top == [4,4,0,0,6,6,2]
+
+def test_call_steal():
+    b1 = Board()
+    b1.bottom[2] = 1
+    b1.bottom[3] = 0
+    assert b1.turn == b1.BOTTOM
+    assert b1.top[2] == 4
+    b1(2)
+    assert b1.bottom[2] == 0
+    assert b1.bottom[3] == 5
+    assert b1.top[2] == 0
+
+
