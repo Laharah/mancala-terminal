5d0cbc86478bbfb4c873c6322171277dfc0de8e7
laharah <laharah22@gmail.com>
2017-01-11 14:57:17 -0800

    Added basic game object

---
+++ mancala/game.py
@@ -0,0 +1,47 @@
+from .board import Board
+
+class Game:
+    'p1 and p2 are functions that return a move when given a board'
+    def __init__(self, player_bottom, player_top):
+        self.board = Board()
+        self.top = player_top
+        self.bottom = player_bottom
+        self.player = {self.board.TOP: self.top, self.board.BOTTOM: self.bottom, None:None}
+        self.player_number = {self.top:Board.TOP, self.bottom:Board.BOTTOM, None:None}
+
+    @property
+    def turn(self):
+        'return the player whos turn it is.'
+        return self.player[self.board.turn]
+
+    @turn.setter
+    def turn(self, value):
+        assert value in (self.top, self.bottom)
+        self.board.turn = Board.TOP if value is self.top else Board.BOTTOM
+
+    @property
+    def score(self):
+        return self.board.score
+
+    @property
+    def game_over(self):
+        return self.turn is None
+
+    @property
+    def winner(self):
+        if not self.game_over:
+            return None
+        score = self.board.score
+        return self.top if score.top < score.bottom else self.bottom
+
+    def __call__(self):
+        board = self.board
+        while board.turn is not None:
+            self.execute_turn()
+
+    def execute_turn(self, player=None):
+        'executes a single turn, optional player to override player function'
+        player = player if player else self.player[self.board.turn]
+        safe_board = Board.from_state(self.board.get_state())
+        move = player(safe_board)
+        self.board(move)
---
+++ mancala/test/test_game.py
@@ -0,0 +1,62 @@
+import pytest
+import random
+from unittest.mock import Mock
+
+from ..game import Game
+from ..board import Board
+
+@pytest.fixture
+def random_player():
+    def p(board):
+        my_side = board.top if board.turn == board.TOP else board.bottom
+        return random.choice([i for i, v in enumerate(my_side[:-1]) if v != 0])
+    return p
+
+def test_calls_players():
+    p1 = Mock(return_value=2)
+    p2 = Mock()
+    game = Game(p1, p2)
+    game.execute_turn(player=p1)
+    assert p1.called
+
+def test_gives_board():
+    p1 = Mock(return_value=2)
+    p2 = Mock()
+    g = Game(p1, p2)
+    g.execute_turn()
+    assert p1.called
+    assert isinstance(p1.call_args[0][0], Board)
+
+def test_gives_to_correct_player():
+    p1 = Mock(return_value=2)
+    p2 = Mock(return_value=2)
+    g = Game(p1, p2)
+    assert g.turn is g.bottom  # p1
+    g.turn = g.top  #set to p2's turn
+    g.execute_turn()
+    assert not p1.called
+    assert p2.called
+
+def test_anti_cheat():
+    def p1(board):
+        if board.turn == board.TOP:
+            my_side = board.top
+        else:
+            my_side = board.bottom
+        my_side[-1] = 100
+        return 2
+    p2 = Mock()
+    g = Game(p1, p2)
+    g.execute_turn()
+    assert g.score == (0, 1)
+
+
+def test_game_loop(random_player):
+    p1 = random_player
+    p2 = random_player
+    g = Game(p1, p2)
+    g()
+    assert g.turn == None
+    assert g.game_over
+    print(g.score)
+
