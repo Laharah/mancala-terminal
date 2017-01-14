76a4211d80838bdd0f6f6a3ed812106e57addd41
laharah <laharah22@gmail.com>
2017-01-13 19:48:10 -0800

    added depth limit to basic bot

--- mancala/player/bot.py
+++ mancala/player/bot.py
@@ -1,30 +1,48 @@
 import functools
 from ..board import State
 
-class Bot:
 
+class Bot:
     def __init__(self):
         self.side = None
         self.mem_cache = {}
         self.utility = self.memo(self.utility)
- 
 
     def memo(self, func):
         @functools.wraps(func)
-        def inner(*args):
+        def inner(*args, **kwargs):
             try:
                 return self.mem_cache[func, args]
             except KeyError:
-                result = self.mem_cache[func, args] = func(*args)
+                result = self.mem_cache[func, args] = func(*args, **kwargs)
                 return result
+
         return inner
 
     @staticmethod
     def available_moves(state):
         yield from (i for i, v in enumerate(state.current_side[:-1]) if v != 0)
 
-    
-    def utility(self, state):
+    def estimate_utility(self, state):
+        'the estimated utility: store points + 1 for every house that can make it to the store'
+        my_side = state.top if self.side == state.TOP else state.bottom
+        other_side = state.bottom if my_side is state.top else state.top
+        my_store = my_side[-1]
+        other_store = other_side[-1]
+        my_store += self.canidate_houses(my_side)
+        other_store += self.canidate_houses(other_side)
+        return (my_store - other_store) / 200
+
+    @staticmethod
+    def canidate_houses(side):
+        size = len(side)
+        canidates = 0
+        for i in range(size):
+            if side[i] >= size - (i + 1):
+                canidates += 1
+        return canidates
+
+    def utility(self, state, max_depth=5):
         if state.turn is None:
             score = state.score
             if score.top > score.bottom:
@@ -40,15 +58,21 @@ class Bot:
             else:
                 return -1
 
-        return self.utility(state.after_move(max(self.available_moves(state), key=lambda m: self.quality(m, state))))
+        if max_depth <= 0 and state.turn != self.side:
+            return self.estimate_utility(state)
 
+        best_move = max(
+            self.available_moves(state),
+            key=lambda m: self.quality(m, state, max_depth=max_depth - 1))
+        return self.utility(state.after_move(best_move), max_depth=max_depth)
 
-    def quality(self, move, state):
-        return self.utility(state.after_move(move))
-
+    def quality(self, move, state, max_depth=4):
+        return self.utility(state.after_move(move), max_depth=max_depth)
 
     def __call__(self, board):
         if self.side is None: self.side = board.turn
+        self.mem_cache = {}
         moves = sorted((self.quality(m, board), m) for m in self.available_moves(board))
         print(moves)
-        return moves[-1][1]
+        top_moves = [m for m in moves if board.after_move(m[1]).turn == self.side]
+        return top_moves[-1][1] if top_moves else moves[-1][1]
--- mancala/test/test_bot.py
+++ mancala/test/test_bot.py
@@ -5,17 +5,19 @@ from ..board import Board
 
 Bot = bot.Bot
 
+
 def test_available_moves():
     bot = Bot()
     board = Board()
     assert list(bot.available_moves(board)) == list(range(6))
-    board.bottom[:3] = [0,0,0]
+    board.bottom[:3] = [0, 0, 0]
     assert list(bot.available_moves(board)) == [3, 4, 5]
+
 
 def test_wants_to_win():
     board = Board()
-    board.top = [4, 0,0,0,0,0,15]
-    board.bottom = [0,0,0,0,2,1,17]
+    board.top = [4, 0, 0, 0, 0, 0, 15]
+    board.bottom = [0, 0, 0, 0, 2, 1, 17]
     bot = Bot()
     expected_moves = [5, 4, 5]
     for expected in expected_moves:
@@ -26,11 +28,11 @@ def test_wants_to_win():
     assert board.score == (19, 20)
     print(bot.mem_cache)
 
-@pytest.mark.skip
+
 def test_limit_break():
     board = Board()
     board.bottom = [6, 4, 2, 3, 1, 1, 0]
-    board.top = [0, 0, 0, 0, 0, 1, 15]
+    board.top = [0, 0, 0, 0, 1, 1, 14]
     bot = Bot()
     expected_moves = [5, 3, 5, 4, 5, 0, 5, 1, 5, 4, 5, 2, 5, 3, 5, 4, 5]
     for expected in expected_moves:
@@ -38,3 +40,9 @@ def test_limit_break():
         assert move == expected
         board(move)
     assert board.score == (16, 17)
+
+
+def test_first_move():
+    board = Board()
+    bot = Bot()
+    assert bot(board) == 2
